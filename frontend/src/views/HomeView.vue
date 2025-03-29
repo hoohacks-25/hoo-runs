ad646530670d823dd6c0925e621169752870d727
Matan Morse <matanbryant@gmail.com>
2025-03-29 13:05:01 -0400

    integrate frontend and backend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <p>Hello, world!</p>
-        <div>Count: {{  store.count  }}</div>
-        <button @click="store.increment">Increment</button>
+        <p>{{ data.route == undefined ? "Undefined" : "Defined"   }}</p>
+
     </div>
 </template>
 
@@ -11,8 +11,19 @@
 
 <script setup>
 import { useMainStore } from '@/stores/store';
+import axios from 'axios'
+import { onMounted, ref, Suspense } from 'vue';
 
 const store = useMainStore();
+
+const data = ref({});
+const generateURL = '/api/run/generate'
+
+const fetchData = async () => {
+    data.value = (await axios.get(generateURL)).data;
+}
+
+onMounted(fetchData);
 </script>
 <style lang="scss" scoped>
 
