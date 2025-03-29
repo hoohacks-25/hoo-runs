e73ea712d2a6a4756910d8db204da90e3d0260a3
MSI Laptop <henrymunprof@gmail.com>
2025-03-29 12:34:18 -0400

    runs route

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,11 @@
 const express = require('express')
 require('dotenv').config();
-
+const runsRoute = require('./routes/runs');
 
 const app = express()
 const port = process.env.PORT || 8080
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use('/run', runsRoute);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
